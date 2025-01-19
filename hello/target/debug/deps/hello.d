27e6fc2c030d6569ae4734fce84f1369cd9ea6f8
aree <whr_zm@163.com>
2025-01-19 17:08:19 +0800

    1

---
+++ hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+D:\DeskTop\RustProjects\hello\target\debug\deps\hello.exe: src\main.rs
+
+D:\DeskTop\RustProjects\hello\target\debug\deps\hello.d: src\main.rs
+
+src\main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+D:\DeskTop\RustProjects\hello\target\debug\hello.exe: D:\DeskTop\RustProjects\hello\src\main.rs
